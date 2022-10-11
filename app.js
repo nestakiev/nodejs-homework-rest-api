7ac66e29c85b3416f9bf867f798ca62dca957ff6
nestakiev <n3st1k13@gmail.com>
2022-10-11 15:32:06 +0300

    fix swagger position

--- app.js
+++ app.js
@@ -17,9 +17,10 @@ app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", authRouter);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
